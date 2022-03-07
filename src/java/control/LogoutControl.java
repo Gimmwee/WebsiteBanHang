96c43deddf9eaf0b5bb29352bf63d7f0433cf705
Your Name <you@example.com>
2022-03-07 23:03:21 +0700

    logoutcontrol

--- src/java/control/LogoutControl.java
+++ src/java/control/LogoutControl.java
@@ -5,12 +5,9 @@
  */
 package control;
 
-import context.DBConnect;
-import dao.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -41,7 +38,6 @@ public class LogoutControl extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-                
-        
+
     }
 }
